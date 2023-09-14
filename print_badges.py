8948fe54e2fb726ed8b69a6d916602facab6ad34
Roberto Verdecchia <roberto.verdecchia@tiscali.it>
2023-09-14 14:44:44 +0200

    1. Add badge, 2. Adjust badge size, 3. Add grey badge border

--- print_badges.py
+++ print_badges.py
@@ -1,7 +1,11 @@
 import pandas as pd
 from reportlab.pdfgen import canvas
-from reportlab.lib.pagesizes import A4
+from reportlab.lib.colors import lightgrey
 from PIL import Image
+
+def cm_to_points(cm):
+    # Convert centimeters to points
+    return cm * 28.3465
 
 def create_badges(csv_file, png_file):
     # Load the CSV file
@@ -10,22 +14,25 @@ def create_badges(csv_file, png_file):
     # Constants for the badges
     badges_per_page = 8
     badges_per_row = 2
-    badge_width, badge_height = A4[0] / badges_per_row, A4[1] / (badges_per_page / badges_per_row)
+    badge_width, badge_height = cm_to_points(8.9), cm_to_points(5.4)  # Adjusted badge size
 
-    # Load the background image and resize it to the badge size
+    # Load the background image
     bg_image = Image.open(png_file)
-    bg_image = bg_image.resize((int(badge_width), int(badge_height)))
 
     # Create a new PDF file
-    c = canvas.Canvas("output.pdf", pagesize=A4)
+    c = canvas.Canvas("output.pdf", pagesize=(badges_per_row * badge_width, (badges_per_page // badges_per_row) * badge_height))
 
     for i, (index, row) in enumerate(df.iterrows()):
         # Calculate the position of the badge on the page
         x = (i % badges_per_row) * badge_width
-        y = A4[1] - ((i // badges_per_row % (badges_per_page // badges_per_row) + 1) * badge_height)
+        y = c._pagesize[1] - ((i // badges_per_row % (badges_per_page // badges_per_row) + 1) * badge_height)
 
-        # Draw the background image
+        # Draw the background image without resizing
         c.drawInlineImage(bg_image, x, y, width=badge_width, height=badge_height)
+
+        # Draw the border
+        c.setStrokeColor(lightgrey)
+        c.rect(x, y, badge_width, badge_height)
 
         # Draw the text with increased size
         c.setFont("Helvetica-Bold", 18)
